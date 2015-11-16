153a6715d10777a64b4539fb50e83f85f0f5ebc9
sdask <manson.serg@gmail.com>
2015-11-16 23:53:15 +0200

    Fix

--- src/main/java/com/uni/python/PythonExecutor.java
+++ src/main/java/com/uni/python/PythonExecutor.java
@@ -12,16 +12,31 @@ public final class PythonExecutor {
 
     public static String executeAndRead(String command) {
         String response;
+        String absolutePath = new File("ser.py").getAbsolutePath();
+        String relativePath = new File("ser.py").getPath();
+        System.out.println("ABS: " + absolutePath);
+        System.out.println("REL: " + relativePath);
+        response = execute(command, absolutePath);
+        if (response == null || response.length() < 1) {
+            response = execute(command, relativePath);
+        }
+        return response;
+    }
+
+    private static String execute(String command, String file) {
+        String response;
         String[] cmd = {
-                "python " + new File("ser.py").getAbsolutePath() + " fds"
+                "python", file, command
         };
         try {
             Process process = Runtime.getRuntime().exec(cmd);
             BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
             StringBuilder sb = new StringBuilder();
             String line;
-            while ((line = br.readLine()) != null) {
-                sb.append(line).append("\n");
+            for (int i = 0; i < 1000; i++) {
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
             }
             response = sb.toString();
         } catch (IOException e) {
