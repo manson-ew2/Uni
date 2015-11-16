58738e91c9d2d2af743c80f31b0e20769ccc98e9
sdask <manson.serg@gmail.com>
2015-11-16 23:41:34 +0200

    Fix

--- src/main/java/com/uni/python/PythonExecutor.java
+++ src/main/java/com/uni/python/PythonExecutor.java
@@ -13,7 +13,7 @@ public final class PythonExecutor {
     public static String executeAndRead(String command) {
         String response;
         String[] cmd = {
-                "python script.py '" + command + "'"
+                "python ser.py '" + command + "'"
         };
         try {
             Process process = Runtime.getRuntime().exec(cmd);
